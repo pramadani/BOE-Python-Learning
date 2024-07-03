1553617291e3726d3d34c7c795e6ab6ebce1f617
luft <araryap@gmail.com>
2024-07-03 07:52:14 +0700

    first commit

---
+++ soal.py
@@ -0,0 +1,37 @@
+data_pribadi = [
+    ["Ahmad", "Jakarta", "1234567890", 15000000],
+    ["Budi", "Malang", "0987654321", 8000000],
+    ["Citra", "Malang", "2468013579", 12000000],
+    ["Dian", "Surabaya", "1357924680", 9500000],
+    ["Eka", "Malang", "3141592653", 11000000],
+    ["Fandi", "Bandung", "9876543210", 13000000],
+    ["Gina", "Malang", "5678901234", 9000000],
+    ["Hendra", "Jakarta", "3456789012", 16000000],
+    ["Indra", "Malang", "6543210987", 10000000],
+    ["Joko", "Surabaya", "7890123456", 11500000],
+    ["Kiki", "Malang", "4321098765", 8500000],
+    ["Lina", "Bandung", "2109876543", 9500000],
+    ["Mega", "Jakarta", "8765432109", 14000000],
+    ["Nina", "Malang", "1098765432", 10500000],
+    ["Oscar", "Surabaya", "5432109876", 12500000],
+    ["Putri", "Malang", "3210987654", 9500000],
+    ["Qori", "Bandung", "6543210987", 11000000],
+    ["Rama", "Jakarta", "8901234567", 13500000],
+    ["Santi", "Malang", "2345678901", 11500000],
+    ["Tono", "Surabaya", "4321098765", 12000000]
+]
+
+penduduk = []
+nama_penduduk = []
+jumlah_penduduk = 0
+rerata_gaji = 0
+
+
+###
+
+
+print("========================================================================")
+print("Penduduk yang tinggal di Malang:\n", nama_penduduk, "\n")
+print("Jumlah orang yang tinggal di Malang:\n", jumlah_penduduk, "\n")
+print("Rata-rata gaji orang di Malang:\n", rerata_gaji)
+print("========================================================================")
