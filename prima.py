ec31ecf1eaf696fe08c04f103f164bb8369bee37
AzaryaSantoso123 <135085971+AzaryaSantoso123@users.noreply.github.com>
2024-07-03 20:58:27 +0700

    soal prima

---
+++ prima.py
@@ -0,0 +1,20 @@
+'''
+Buatlah kode untuk mencari bilangan prima yang ada di antara angka 1-100
+
+Output: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+
+
+'''
+#Salah satu solusi yang bisa dipakai
+
+def isPrima(x):
+    #Tulis Kode Disini
+    pass #nanti line ini dihapus ya
+
+listPrima = []
+
+for x in #berikan kondisi :
+    #Lanjutkan kode disini
+
+print(listPrima)
