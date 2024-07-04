15170c66f4afd3cf45e25f6ec1d51373d9744eaa
Ararya Pramadani <87419268+pramadani@users.noreply.github.com>
2024-07-04 07:47:52 +0700

    Create hitung.py

---
+++ hitung.py
@@ -0,0 +1,25 @@
+###
+
+
+###
+
+def hitung(..., ..., ...):
+    if ... == ...:
+        return tambah(..., ...)
+    
+    elif ... == ...:
+        return kurang(..., ...)
+    
+    elif ... == ...:
+        return kali(..., ...)
+    
+    elif ... == ...:
+        return bagi(..., ...)
+    
+    else:
+        return "Operasi tidak valid"
+
+print("Hasil penambahan:", hitung('+', 10, 5))
+print("Hasil pengurangan:", hitung('-', 10, 5))
+print("Hasil perkalian:", hitung('*', 10, 5))
+print("Hasil pembagian:", hitung('/', 10, 5))
